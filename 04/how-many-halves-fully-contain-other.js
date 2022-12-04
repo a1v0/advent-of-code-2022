3caf20d73763cee76906483186da55d1c6df3444
avc285 <avc285@gmail.com>
2022-12-04 09:52:24 +0000

    plan approach

---
+++ 04/how-many-halves-fully-contain-other.js
@@ -0,0 +1,6 @@
+const input = require("./input.js");
+
+// split into pairs
+// split each pair into nested array with two strings
+// map each item into a string of the full range, e.g. 1-4 => 1234
+// count how often one string is included in other
