b962efc0e115065bac38668d375ffaf5c10e8174
avc285 <avc285@gmail.com>
2022-12-05 19:07:12 +0000

    complete second task

---
+++ 05/using-cratemover-9001.js
@@ -0,0 +1,24 @@
+// This is the same code as in the first task, except that I do not reverse the spliced array before adding it to the stack
+
+const { stacks, moves: movesString } = require("./input.js");
+
+// split moves into array of objects { quantity: x, from: y, to: z }
+const moveStrings = movesString.split("\n");
+const moves = moveStrings.map((moveString) => {
+    const numbers = moveString.match(/\d+/g);
+    return { quantity: numbers[0], from: numbers[1], to: numbers[2] };
+});
+
+// loop through moves array, creating a slice/splice (whichever one it is) based on the command
+moves.forEach((move) => {
+    const removedElements = stacks[move.from - 1].splice(0, move.quantity);
+    stacks[move.to - 1].splice(0, 0, ...removedElements);
+});
+
+// loop through stacks, creating a string of [0] of each stack
+let firstItems = "";
+stacks.forEach((stack) => {
+    firstItems += stack[0];
+});
+
+console.log(firstItems);
