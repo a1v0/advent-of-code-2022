83f610582994baca4ea01d6a6c1600fbf3937b64
avc285 <avc285@gmail.com>
2022-12-06 13:14:51 +0000

    undo export

--- 06/start-of-packet-marker.js
+++ 06/start-of-packet-marker.js
@@ -34,5 +34,3 @@ for (let i = 0; i < groupsOfFour.length; ++i) {
 
 // log index of the first substring that passes the test, then return index + 4
 console.log(startOfPacketIndex + 4);
-
-module.exports = groupsOfFour;
