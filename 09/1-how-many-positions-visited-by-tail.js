49c76d74c4556a421f222fd7912b6f7628c7130d
avc285 <avc285@gmail.com>
2022-12-09 13:35:37 +0000

    plan approach

---
+++ 09/1-how-many-positions-visited-by-tail.js
@@ -0,0 +1,7 @@
+const { input: testInput } = require("./input");
+
+// split input into lines of instructions
+// map instructions into two-part arrays
+// create an object to hold all locations visited by tail (something like this: { [x,y]:true, [a,b]:true })
+// create object to house latest x/y coordinates of head and tail
+// loop through instructions, updating objects as I go
