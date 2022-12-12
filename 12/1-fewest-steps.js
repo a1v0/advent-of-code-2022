287734862389ede8bba2d9a2c9129ebc2025711e
avc285 <avc285@gmail.com>
2022-12-12 11:28:56 +0000

    plan approach

---
+++ 12/1-fewest-steps.js
@@ -0,0 +1,9 @@
+const { testInput: input } = require("./input");
+
+// split input into rows
+// create string listing all letters
+// create array to house all routes
+// identify coordinates of start
+// create recursive function to move through array from a specific location
+// // add any complete route to routes array
+// once routes have been assembled, find shortest one (it might be possible to simply return the first route that is returned by the recursive function)
