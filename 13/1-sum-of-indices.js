737d6eeb21cbd603091314edadd0cdb3c90e387a
avc285 <avc285@gmail.com>
2022-12-13 13:31:47 +0000

    plan approach

---
+++ 13/1-sum-of-indices.js
@@ -0,0 +1,10 @@
+const { testInput: input } = require("./input");
+
+// split input into pairs of arrays
+// map pairs into pairs of arrays using eval()
+// create counter for amount of stuff in right order
+// make a comparison function that can be called recursively, given two array inputs
+// loop through each pair of arrays, comparing each item
+// // if item is not an array, compare sizes
+// // if one item is an array but other isn't, wrap other in [] and call the recursive function on it
+// return number of things in right order
