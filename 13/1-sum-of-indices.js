6a921e2d4679ed2054120aa78fe3132f5e13b78e
avc285 <avc285@gmail.com>
2022-12-15 13:19:43 +0000

    re-plan approach

--- 13/1-sum-of-indices.js
+++ 13/1-sum-of-indices.js
@@ -1,10 +1,54 @@
 const { testInput: input } = require("./input");
 
-// split input into pairs of arrays
-// map pairs into pairs of arrays using eval()
-// create counter for amount of stuff in right order
-// make a comparison function that can be called recursively, given two array inputs
-// loop through each pair of arrays, comparing each item
-// // if item is not an array, compare sizes
-// // if one item is an array but other isn't, wrap other in [] and call the recursive function on it
-// return number of things in right order
+// THIS NEEDS BETTER PLANNING. CURRENT IMPLEMENTATION IS TOO MESSILY DESIGNED
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// split input into pairs of strings
+// map pairs into an array of arrays, using eval()
+// create counter for stuff in right order
+// create compare function take takes two arrays and index+1 as argument
+// // if both are numbers, compare
+// // // if left > right, break without adding to counter
+// // // if left < right, add to counter, then break
+// // // else continue
+// // if one is array
+// // // coerce other to array (if it isn't already an array)
+// // // run function recursively, returning something (what/how?) if the tests fail/pass
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
