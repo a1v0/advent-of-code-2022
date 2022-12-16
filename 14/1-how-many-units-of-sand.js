027f7d254f381b2ff4aa39957accda4d22176139
avc285 <avc285@gmail.com>
2022-12-16 18:55:09 +0000

    complete first task

--- 14/1-how-many-units-of-sand.js
+++ 14/1-how-many-units-of-sand.js
@@ -1,4 +1,4 @@
-const { testInput: input } = require("./input");
+const { input } = require("./input");
 
 // split input into array of rows
 const blockersStrings = input.split("\n");
@@ -19,14 +19,28 @@ const blockers = blockersStrings.map((blockersString) => {
 // create array to house all blocked coordinates
 const blockedCoords = [];
 
+// store extremities of map
+let leftmost = 500,
+    rightmost = 500,
+    lowest = 0;
+
 // loop through rows, calculating every blocked coordinate and adding it to array
 blockers.forEach((blocker) => {
     for (let i = 0; i < blocker.length - 1; ++i) {
         addBlockers(
             blocker[i],
             blocker[i + 1],
-            blocker[i][0] !== blocker[i + 1][0] ? 0 : 1 // this may cause trouble if there are 'lines' with just one element. In case of errors, stick another ternary on the back of it
+            blocker[i][0] !== blocker[i + 1][0] ? 0 : 1
         );
+
+        // identify extremities of grid
+        if (blocker[i][0] < leftmost) leftmost = blocker[i][0];
+        if (blocker[i + 1][0] < leftmost) leftmost = blocker[i + 1][0];
+        if (blocker[i][0] > rightmost) rightmost = blocker[i][0];
+        if (blocker[i + 1][0] > rightmost) rightmost = blocker[i + 1][0];
+
+        if (blocker[i][1] > lowest) lowest = blocker[i][1];
+        if (blocker[i + 1][1] > lowest) lowest = blocker[i + 1][1];
     }
 });
 
@@ -34,6 +48,7 @@ function addBlockers(start, end, xOrY) {
     // needs to take into account whether it goes up, down, left or right
     const higherNumber = start[xOrY] < end[xOrY] ? end : start;
     const lowerNumber = start[xOrY] < end[xOrY] ? start : end;
+
     for (let i = 0; i <= higherNumber[xOrY] - lowerNumber[xOrY]; ++i) {
         const blockedCoord =
             xOrY === 0
@@ -45,11 +60,34 @@ function addBlockers(start, end, xOrY) {
     }
 }
 
-console.log(blockers);
-console.log(blockedCoords);
-
-// identify (HOW?) where the edges of the grid are, to find out where a grain of sand needs to be before it tumbles into the abyss
 // create a counter to count resting grains of sand
+let restingGrainsCounter = 0;
+
 // make a while loop to drop sand until a grain of sand has reached the abyss
-// nest a while loop inside to move a grain until it comes to rest
+let currentX = 500,
+    currentY = 0;
+
+while (currentX >= leftmost && currentX <= rightmost && currentY < lowest) {
+    if (!blockedCoords.includes(`${currentX},${currentY + 1}`)) {
+        ++currentY;
+        continue;
+    } else if (!blockedCoords.includes(`${currentX - 1},${currentY + 1}`)) {
+        --currentX;
+        ++currentY;
+        continue;
+    } else if (!blockedCoords.includes(`${currentX + 1},${currentY + 1}`)) {
+        ++currentX;
+        ++currentY;
+        continue;
+    }
+
+    if (currentX >= leftmost && currentX <= rightmost && currentY < lowest) {
+        blockedCoords.push(`${currentX},${currentY}`);
+        ++restingGrainsCounter;
+        currentX = 500;
+        currentY = 0;
+    }
+}
+
 // return total amount of resting sand grains at the end
+console.log(restingGrainsCounter);
