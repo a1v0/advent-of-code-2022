dc944a15dbb0a6f4f3db706e971528b44446191a
avc285 <avc285@gmail.com>
2022-12-16 08:32:14 +0000

    plan approach

---
+++ 14/1-how-many-units-of-sand.js
@@ -0,0 +1,11 @@
+const { testInput: input } = require("./input");
+
+// split input into array of rows
+// split each row into a selection of coordinates
+// create array to house all blocked coordinates
+// loop through rows, calculating every blocked coordinate and adding it to array
+// identify (HOW?) where the edges of the grid are, to find out where a grain of sand needs to be before it tumbles into the abyss
+// create a counter to count resting grains of sand
+// make a while loop to drop sand until a grain of sand has reached the abyss
+// nest a while loop inside to move a grain until it comes to rest
+// return total amount of resting sand grains at the end
