3083546ab92d5fe0bab83c37827ba49cd246912a
avc285 <avc285@gmail.com>
2022-12-18 21:10:19 +0000

    prepare everything minus recursive function

--- 16/1-what-is-the-most-pressure.js
+++ 16/1-what-is-the-most-pressure.js
@@ -15,17 +15,19 @@ const valves = valvesStrings.map((valveString) => {
 });
 
 // create sort callback function, to put all flowing valves at the end of the array, and otherwise sort valves by flow rate (descending)
-function sortValves(a, b) {
-    if (a.isFlowing) return 1;
-    if (b.isFlowing) return -1;
-    return b.flowRate - a.flowRate;
+function sortValves() {
+    valves.sort((a, b) => {
+        if (a.isFlowing) return 1;
+        if (b.isFlowing) return -1;
+        return b.flowRate - a.flowRate;
+    });
 }
+sortValves();
 
-valves.sort(sortValves);
 console.log(valves);
 
 // calculate current flow
-function findFlowRate() {
+function getCurrentFlow() {
     let currentFlow = 0;
     valves.forEach(({ isFlowing, flowRate }) => {
         if (isFlowing) {
@@ -42,24 +44,31 @@ function getValve(valveID) {
     });
 }
 
-// create function to find shortest path from current object to first object in sorted array
-// // turn on valve once you reach it
-// // sort array (this can be done elsewhere in the code, too)
-// start at AA
+let currentValve = "AA";
+let totalFlow = 0;
+let clock = 0;
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+while (clock <= 30) {
+    // identify next stop
+    const nextStop = findNextValve(currentValve);
 
-// find shortest way from AA to largest valve
-// look for next best path (a path whose flowRate-distance is highest), so long as we've never been there before
-// find shortest way from largest valve to next largest etc. (find a way to prevent going to places that have already been visited)
+    // update current valve, clock and flow
+    currentValve = nextStop.valve;
+    clock += nextStop.distance + 1; // +1 to account for switching on the valve
+    totalFlow += getCurrentFlow() * (nextStop.distance + 1);
+
+    // update flowing status and sort valves
+    getValve(currentValve).isFlowing = true; // this must be set to true AFTER the flow has been calculated
+    sortValves();
+}
+
+function findNextValve(currentValve) {
+    // recursively or otherwise, go through all valves, in descending order of flowRate
+    // find how many steps it would take to get to the highest-flow valve, and subtract this from the valve's flow
+    // e.g. valve's flow is 20, and it takes 7 steps to get there, so total score is 20 - 7 = 13
+    // go to the valve with the highest score, returing it as { valve: "AA", distance: Number }
+
+    return { valve: "AA", distance: Number };
+}
+
+console.log(totalFlow);
