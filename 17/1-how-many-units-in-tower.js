72e89b92ac6cce6a43bbd8fede0d2aab406180b4
avc285 <avc285@gmail.com>
2022-12-21 21:54:18 +0000

    draft approach

---
+++ 17/1-how-many-units-in-tower.js
@@ -0,0 +1,10 @@
+const { testInput: input } = require("./input");
+
+// create functions that create each of the rocks (a rock should be an array of coordinates)
+// // should take an argument of coordinate of lowest square
+// create an array to house all resting coordinates
+// create a counter for rocks
+// while loop counter < 2022
+// create shape using switch (counter % 5)
+// create counter to count how many instructions from input have been used
+// nest a while loop to go through instructions until rock comes to rest
