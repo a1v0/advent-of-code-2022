ad69abe4f8e90e76920dcbccb143b2e397ab6c98
avc285 <avc285@gmail.com>
2023-01-08 10:37:36 +0000

    write first test

---
+++ __tests__/aoc.test.js
@@ -0,0 +1,18 @@
+const { day20Task1 } = require("../20/1-sum-three-numbers-grove-coordinates");
+const { testInput: input } = require("../20/input");
+
+describe("Day 20", () => {
+    describe("Task 1", () => {
+        test("should return object with property of numbersStrings, returning data according to sample input", () => {
+            const numbersStrings = day20Task1.numbersStrings;
+            expect(numbersStrings[0]).toBe("1, 2, -3, 3, -2, 0, 4");
+            expect(numbersStrings[1]).toBe("2, 1, -3, 3, -2, 0, 4");
+            expect(numbersStrings[2]).toBe("1, -3, 2, 3, -2, 0, 4");
+            expect(numbersStrings[3]).toBe("1, 2, 3, -2, -3, 0, 4");
+            expect(numbersStrings[4]).toBe("1, 2, -2, -3, 0, 3, 4");
+            expect(numbersStrings[5]).toBe("1, 2, -3, 0, 3, 4, -2");
+            expect(numbersStrings[6]).toBe("1, 2, -3, 0, 3, 4, -2");
+            expect(numbersStrings[7]).toBe("1, 2, -3, 4, 0, 3, -2");
+        });
+    });
+});
