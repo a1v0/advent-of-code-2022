27ed22344ea70d782fd552e82acdb90e950c963a
avc285 <avc285@gmail.com>
2023-01-09 08:50:51 +0000

    create simple unit test

--- 21/1-what-number-will-root-yell.js
+++ 21/1-what-number-will-root-yell.js
@@ -1,10 +1,13 @@
 const { testInput: input } = require("./input");
 
-// split input into array of strings
-// map strings into objects { name: "abcd", operation: "abcd+abcd"}
-// create root string, starting out as the value of root from the input
-// create a regex that finds all four-letter groups in the root string
-// while loop until the regex doesn't find anything anymore
-// // loop through each four-letter group in the regex array
-// // replace each four-letter group with "( whatever expression corresponds to that group )"
-// once the string has been composed, use eval()
+function day21Task1(input) {
+    // split input into array of strings
+    // map strings into dictionary { "abcd": "efgh + ijkl" }
+    // create root string, starting out as the value of root from the input
+    // create a regex that finds all four-letter groups in the root string
+    // while loop until the regex doesn't find anything anymore
+    // // loop through each four-letter group in the regex array
+    // // replace each four-letter group with "( whatever expression corresponds to that group )"
+    // once the string has been composed, use eval()
+}
+module.exports = { day21Task1 };
--- __tests__/aoc.test.js
+++ __tests__/aoc.test.js
@@ -1,11 +1,13 @@
 const { day20Task1 } = require("../20/1-sum-three-numbers-grove-coordinates");
 const { day20Task2 } = require("../20/2-sum-after-ten-rounds");
-const { testInput: input } = require("../20/input");
+const { day21Task1 } = require("../21/1-what-number-will-root-yell");
+const { testInput: day20Input } = require("../20/input");
+const { testInput: day21Input } = require("../21/input");
 
 describe("Day 20", () => {
     describe("Task 1", () => {
         test("should return object with property of numbersStrings, returning data according to sample input", () => {
-            const numbersStrings = day20Task1(input).numbersStrings;
+            const numbersStrings = day20Task1(day20Input).numbersStrings;
             expect(numbersStrings[0]).toBe("1, 2, -3, 3, -2, 0, 4");
             expect(numbersStrings[1]).toBe("2, 1, -3, 3, -2, 0, 4");
             expect(numbersStrings[2]).toBe("1, -3, 2, 3, -2, 0, 4");
@@ -16,19 +18,19 @@ describe("Day 20", () => {
             expect(numbersStrings[7]).toBe("1, 2, -3, 4, 0, 3, -2");
         });
         test("should return the correct 1000th, 2000th and 3000th values", () => {
-            const { oneThou, twoThou, threeThou } = day20Task1(input);
+            const { oneThou, twoThou, threeThou } = day20Task1(day20Input);
             expect(oneThou).toBe(4);
             expect(twoThou).toBe(-3);
             expect(threeThou).toBe(2);
         });
         test("should return correct sumOfThree value", () => {
-            const sumOfThree = day20Task1(input).sumOfThree;
+            const sumOfThree = day20Task1(day20Input).sumOfThree;
             expect(sumOfThree).toBe(3);
         });
     });
     describe("Task 2", () => {
         test("should return object with correct values after each round of mixing", () => {
-            const numbersStrings = day20Task2(input).numbersStrings;
+            const numbersStrings = day20Task2(day20Input).numbersStrings;
             expect(numbersStrings[0]).toBe(
                 "811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612"
             );
@@ -64,14 +66,22 @@ describe("Day 20", () => {
             );
         });
         test("should return the correct 1000th, 2000th and 3000th values", () => {
-            const { oneThou, twoThou, threeThou } = day20Task2(input);
+            const { oneThou, twoThou, threeThou } = day20Task2(day20Input);
             expect(oneThou).toBe(811589153);
             expect(twoThou).toBe(2434767459);
             expect(threeThou).toBe(-1623178306);
         });
         test("should return correct sumOfThree value", () => {
-            const sumOfThree = day20Task2(input).sumOfThree;
+            const sumOfThree = day20Task2(day20Input).sumOfThree;
             expect(sumOfThree).toBe(1623178306);
+        });
+    });
+});
+
+describe.only("Day 21", () => {
+    describe("Task 1", () => {
+        test("returns correct value", () => {
+            expect(day21Task1(day21Input)).toBe(152);
         });
     });
 });
