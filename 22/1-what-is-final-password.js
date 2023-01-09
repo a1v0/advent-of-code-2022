e00ff7d4c9843b699f3ca3a574c5727d3697bff9
avc285 <avc285@gmail.com>
2023-01-09 18:38:30 +0000

    pass unit test

--- 22/1-what-is-final-password.js
+++ 22/1-what-is-final-password.js
@@ -22,19 +22,137 @@ function day22Task1(input) {
     });
 
     // identify starting coordinates
-    // create var to chart direction. It'll be a counter, and its value % 4 defines direction (right: 0, down: 1, left: 2, up: 3)
-    // loop through instructions
-    // // if instruction isNaN, change direction
-    // // else, move
-    // // if you go off the grid horizontally, wrap around
-    // // if you go off the grid vertically, wrap around
-    // // if you hit a # (even if this is on the other side of the wrap), stay where you are and stop the current move
-    // retrieve current row + 1 (rows in the task aren't zero-indexed) * 1000
-    // retrieve current column + 1 (columns in the task aren't zero-indexed) * 4
-    // retrieve current direction value (direction % 4)
-    // return sum of these
+    const startingX = board[0].indexOf(".");
+    const currentCoordinates = [startingX, 0];
 
-    return;
+    // create var to chart direction. It'll be a counter, and its value % 4 defines direction (right: 0, down: 1, left: 2, up: 3)
+    let direction = 0;
+
+    // loop through instructions
+    for (let instruction of instructions) {
+        // if instruction isNaN, change direction
+        if (isNaN(instruction)) {
+            direction += instruction === "L" ? -1 : 1;
+            continue;
+        }
+
+        // else, move
+        move(instruction, direction, currentCoordinates, board);
+    }
+
+    const multipliedRow = (currentCoordinates[1] + 1) * 1000, // +1 because rows in the task aren't zero-indexed
+        multipliedColumn = (currentCoordinates[0] + 1) * 4, // +1 because columns in the task aren't zero-indexed
+        directionValue = direction % 4;
+
+    return multipliedRow + multipliedColumn + directionValue;
 }
 // console.log(day22Task1(input));
+
+function move(instruction, direction, currentCoordinates, board) {
+    const directionValue = direction % 4;
+
+    // this code is very repetitive. I'd love to know how to reduce that
+    for (let i = 0; i < instruction; ++i) {
+        // going right
+        if (directionValue === 0) {
+            if (
+                board[currentCoordinates[1]][currentCoordinates[0] + 1] === "."
+            ) {
+                // if it's a valid move
+                ++currentCoordinates[0];
+            } else if (
+                board[currentCoordinates[1]][currentCoordinates[0] + 1] !== "#"
+            ) {
+                // if it's undefined or a space, do a wrap, provided there's not # on the other side
+                if (
+                    board[currentCoordinates[1]].indexOf(".") <
+                    board[currentCoordinates[1]].indexOf("#")
+                ) {
+                    currentCoordinates[0] =
+                        board[currentCoordinates[1]].indexOf(".");
+                }
+            } else {
+                // if it's a hash
+                break;
+            }
+            continue;
+        }
+
+        // going left
+        if (directionValue === 2) {
+            if (
+                board[currentCoordinates[1]][currentCoordinates[0] - 1] === "."
+            ) {
+                // if it's a valid move
+                --currentCoordinates[0];
+            } else if (
+                board[currentCoordinates[1]][currentCoordinates[0] - 1] !== "#"
+            ) {
+                // if it's undefined or a space, do a wrap, provided there's not # on the other side
+                if (
+                    board[currentCoordinates[1]].lastIndexOf(".") >
+                    board[currentCoordinates[1]].lastIndexOf("#")
+                ) {
+                    currentCoordinates[0] =
+                        board[currentCoordinates[1]].lastIndexOf(".");
+                }
+            } else {
+                // if it's a hash
+                break;
+            }
+            continue;
+        }
+
+        // going down
+        if (directionValue === 1) {
+            if (
+                board[currentCoordinates[1] + 1][currentCoordinates[0]] === "."
+            ) {
+                // if it's a valid move
+                ++currentCoordinates[1];
+            } else if (
+                board[currentCoordinates[1] + 1][currentCoordinates[0]] !== "#"
+            ) {
+                for (let j = 0; j < board.length; ++j) {
+                    if (board[j][currentCoordinates[0]] === "#") {
+                        break;
+                    } else if (board[j][currentCoordinates[0]] === ".") {
+                        currentCoordinates[1] = j;
+                        break;
+                    }
+                }
+            } else {
+                // if it's a hash
+                break;
+            }
+            continue;
+        }
+
+        // going up
+        if (directionValue === 3) {
+            if (
+                board[currentCoordinates[1] - 1][currentCoordinates[0]] === "."
+            ) {
+                // if it's a valid move
+                --currentCoordinates[1];
+            } else if (
+                board[currentCoordinates[1] - 1][currentCoordinates[0]] !== "#"
+            ) {
+                for (let j = board.length - 1; j >= 0; --j) {
+                    if (board[j][currentCoordinates[0]] === "#") {
+                        break;
+                    } else if (board[j][currentCoordinates[0]] === ".") {
+                        currentCoordinates[1] = j;
+                        break;
+                    }
+                }
+            } else {
+                // if it's a hash
+                break;
+            }
+            continue;
+        }
+    }
+}
+
 module.exports = { day22Task1 };
