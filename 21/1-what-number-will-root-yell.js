bd4c77895d950b252eed37973aaa083625cb25fd
avc285 <avc285@gmail.com>
2023-01-09 12:04:27 +0000

    refactor solution

--- 21/1-what-number-will-root-yell.js
+++ 21/1-what-number-will-root-yell.js
@@ -1,15 +1,6 @@
 const { input } = require("./input");
 
-//
-//
-// solution is very slow with the real data. Presumably string.replace() isn't a fast way to do this
-// I could see if this refactor would work:
-// // go through the monkeys object, finding each index with a number
-// // loop through until everything property has a number, then eval() the root property
-//
-//
-
-function day21Task1(input) {
+function tempStorage(input) {
     // split input into array of strings
     const inputStrings = input.split("\n");
 
@@ -39,6 +30,57 @@ function day21Task1(input) {
     }
     // once the string has been composed, use eval()
     return eval(root);
+}
+
+function day21Task1(input) {
+    //
+    //
+    // solution is very slow with the real data. Presumably string.replace() isn't a fast way to do this
+    // I could see if this refactor would work:
+    // // go through the monkeys object, finding each index with a number
+    // // loop through until everything property has a number, then eval() the root property
+    //
+    //
+
+    // split input into array of strings
+    const inputStrings = input.split("\n");
+
+    // map strings into object
+    const monkeys = {};
+    inputStrings.forEach((inputString) => {
+        const monkeyName = inputString.match(/^\w{4}/)[0];
+        let monkeyCry = inputString.match(/(?<=:\s).*$/)[0];
+        // const isNumber = !isNaN(Number(monkeyCry));
+        monkeys[monkeyName] = [monkeyCry, false];
+    });
+
+    // while loop until root item is a number
+    const quartetRegex = /\w{4}/;
+    while (!monkeys.root[1]) {
+        // loop through monkeys, replacing strings as necessary
+        for (let quartet in monkeys) {
+            // check if a quartet still exists in monkeyCry
+            if (
+                !quartetRegex.test(monkeys[quartet][0]) &&
+                !monkeys[quartet][1]
+            ) {
+                // use eval() to update object, set isNumber to true
+                monkeys[quartet][0] = String(eval(monkeys[quartet][0]));
+                monkeys[quartet][1] = true;
+
+                // find quartet that uses that value and update
+                for (let quartetSearch in monkeys) {
+                    if (RegExp(quartet).test(monkeys[quartetSearch][0])) {
+                        monkeys[quartetSearch][0] = monkeys[
+                            quartetSearch
+                        ][0].replace(quartet, monkeys[quartet][0]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return Number(monkeys.root[0]);
 }
 day21Task1(input);
 module.exports = { day21Task1 };
