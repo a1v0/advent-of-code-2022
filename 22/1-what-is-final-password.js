fcdf19a27cd7282af4d57d826b8dc9ac1ea2e3b4
avc285 <avc285@gmail.com>
2023-01-09 17:44:29 +0000

    plan approach

--- 22/1-what-is-final-password.js
+++ 22/1-what-is-final-password.js
@@ -1,7 +1,40 @@
 const { input } = require("../22/input");
 
 function day22Task1(input) {
+    // use regex to extract instructions and board from input
+    const instructionsString = input.match(/\w+/)[0];
+    const instructions = instructionsString
+        .match(/\d+|\D/g)
+        .map((instruction) => {
+            if (!isNaN(Number(instruction))) {
+                return Number(instruction);
+            }
+            return instruction;
+        });
+
+    const boardString = input.match(/^[\s\#\.!]+(?=\n\n)/)[0];
+
+    // split board into rows and columns
+    const board = [];
+    const boardRows = boardString.split("\n");
+    boardRows.forEach((boardRow) => {
+        board.push(boardRow.split(""));
+    });
+
+    // identify starting coordinates
+    // create var to chart direction. It'll be a counter, and its value % 4 defines direction (right: 0, down: 1, left: 2, up: 3)
+    // loop through instructions
+    // // if instruction isNaN, change direction
+    // // else, move
+    // // if you go off the grid horizontally, wrap around
+    // // if you go off the grid vertically, wrap around
+    // // if you hit a # (even if this is on the other side of the wrap), stay where you are and stop the current move
+    // retrieve current row + 1 (rows in the task aren't zero-indexed) * 1000
+    // retrieve current column + 1 (columns in the task aren't zero-indexed) * 4
+    // retrieve current direction value (direction % 4)
+    // return sum of these
+
     return;
 }
-
+// console.log(day22Task1(input));
 module.exports = { day22Task1 };
