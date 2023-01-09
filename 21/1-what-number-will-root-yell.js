ec2dda22cd52a3e2d77aa727acc51812a445088c
avc285 <avc285@gmail.com>
2023-01-09 12:28:31 +0000

    complete first task

--- 21/1-what-number-will-root-yell.js
+++ 21/1-what-number-will-root-yell.js
@@ -1,47 +1,6 @@
 const { input } = require("./input");
 
-function tempStorage(input) {
-    // split input into array of strings
-    const inputStrings = input.split("\n");
-
-    // map strings into dictionary { "abcd": "efgh + ijkl" }
-    const monkeys = {};
-    inputStrings.forEach((inputString) => {
-        const monkeyName = inputString.match(/^\w{4}/)[0];
-        // at this point it doesn't matter that numbers are being returned as strings
-        monkeys[monkeyName] = inputString.match(/(?<=:\s).*$/)[0];
-    });
-
-    // create root string, starting out as the value of root from the input
-    let root = monkeys.root;
-
-    // create a regex that finds all four-letter groups in the root string
-    const letterQuartetRegex = /\w{4}/g;
-
-    // while loop until the regex doesn't find anything anymore
-    while (letterQuartetRegex.test(root)) {
-        // loop through each four-letter group in the regex array
-        const quartets = root.match(letterQuartetRegex);
-        console.log(quartets.length);
-        quartets.forEach((quartet) => {
-            // replace each four-letter group with "( whatever expression corresponds to that group )"
-            root = root.replace(quartet, `(${monkeys[quartet]})`);
-        });
-    }
-    // once the string has been composed, use eval()
-    return eval(root);
-}
-
 function day21Task1(input) {
-    //
-    //
-    // solution is very slow with the real data. Presumably string.replace() isn't a fast way to do this
-    // I could see if this refactor would work:
-    // // go through the monkeys object, finding each index with a number
-    // // loop through until everything property has a number, then eval() the root property
-    //
-    //
-
     // split input into array of strings
     const inputStrings = input.split("\n");
 
@@ -50,7 +9,6 @@ function day21Task1(input) {
     inputStrings.forEach((inputString) => {
         const monkeyName = inputString.match(/^\w{4}/)[0];
         let monkeyCry = inputString.match(/(?<=:\s).*$/)[0];
-        // const isNumber = !isNaN(Number(monkeyCry));
         monkeys[monkeyName] = [monkeyCry, false];
     });
 
@@ -59,11 +17,7 @@ function day21Task1(input) {
     while (!monkeys.root[1]) {
         // loop through monkeys, replacing strings as necessary
         for (let quartet in monkeys) {
-            // check if a quartet still exists in monkeyCry
-            if (
-                !quartetRegex.test(monkeys[quartet][0]) &&
-                !monkeys[quartet][1]
-            ) {
+            try {
                 // use eval() to update object, set isNumber to true
                 monkeys[quartet][0] = String(eval(monkeys[quartet][0]));
                 monkeys[quartet][1] = true;
@@ -77,10 +31,10 @@ function day21Task1(input) {
                         break;
                     }
                 }
-            }
+            } catch {}
         }
     }
     return Number(monkeys.root[0]);
 }
-day21Task1(input);
+console.log(day21Task1(input));
 module.exports = { day21Task1 };
