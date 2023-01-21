7218881473d7c45676f009272ccc91a68abe1781
avc285 <avc285@gmail.com>
2023-01-21 18:46:55 +0000

    plan approach for second task

---
+++ 21/2-what-is-value-of-humn.js
@@ -0,0 +1,7 @@
+// turn input into object, as in Task 1
+// store both four-letter groups marked in root
+// evaluate everything you can without knowing value of humn
+// this should yield two expressions, each containing humn as an unknown value
+// let humn = 0 (assuming that values will only ever be positive)
+// while loop to run until both expressions are equal
+// return value of humn
