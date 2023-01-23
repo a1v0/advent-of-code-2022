184e9269ae564f94e31a3a4102033e1b1d8bc09f
avc285 <avc285@gmail.com>
2023-01-23 17:08:51 +0000

    make progress on boilerplate

--- 23/1-how-many-empty-ground-tiles.js
+++ 23/1-how-many-empty-ground-tiles.js
@@ -2,10 +2,47 @@ const { input } = require("./input");
 
 function day23Task1(input) {
     // split input into rows
+    const rowsStrings = input.split("\n");
+
     // create array to store elves
+    const elves = [];
+
     // map rows into this format [null (if empty), reference to elf object]
-    // create elf objects { directions: [], position: [x, y], proposal: "x,y" }, push them to elves array and add reference to it in rows array
-    // create function to identify occupied positions surrounding elf
+    const groveMap = rowsStrings.map((rowString, rowNo) => {
+        const row = [];
+        for (let i = 0; i < rowString.length; ++i) {
+            if (rowString[i] === ".") {
+                row.push(null);
+            } else {
+                // create elf objects { directions: [], position: [x, y], proposal: "x,y" }, push them to elves array and add reference to it in rows array
+                elves.unshift({
+                    directions: [],
+                    //
+                    //
+                    //
+                    //
+                    // INSERT REFERENCE TO ALL FOUR FUNCTIONS HERE
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    //
+                    position: [i, rowNo]
+                });
+                row.push(elves[0]);
+            }
+        }
+        return row;
+    });
+    console.log(groveMap);
+
     // create four checker functions that return true if the elf should propose to go somewhere
     // for loop to go through procedure ten times
     // create empty moves object
@@ -20,3 +57,9 @@ function day23Task1(input) {
 }
 // console.log(day23Task1(input));
 module.exports = { day23Task1 };
+
+// function to identify occupied positions surrounding elf
+function checkOccupiedPositions(elf) {
+    // returns null if all surrounding positions are empty
+    // or an object { N: true, NE: false, ... }
+}
