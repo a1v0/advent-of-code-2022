828b32031eb2d889538ccb1a565624e855354c1a
a1v0 <avc285@gmail.com>
2023-05-31 13:08:19 +0100

    Remove comments

--- 11/1-monkey-business-after-twenty-rounds.js
+++ 11/1-monkey-business-after-twenty-rounds.js
@@ -1,13 +1,10 @@
 const { input } = require("./input");
 
-// split string into different monkeys
 const monkeysStrings = input.split("\n\n");
 
-// map monkeys into monkey objects (incl. a new 'objectsInspected' property)
 const monkeys = monkeysStrings.map((monkeyString) => {
     const monkey = { objectsInspected: 0 };
 
-    // get starting items
     const startingItemsRegex = /(?<=Starting items: )[\d+,*\s]+(?=\n)/;
     const startingItemsString = monkeyString.match(startingItemsRegex)[0];
     const startingItemsStrings = startingItemsString.split(", ");
@@ -15,15 +12,12 @@ const monkeys = monkeysStrings.map((monkeyString) => {
         return Number(startingItemsString);
     });
 
-    // operation (will take a string and evaluate it later)
     const operationRegex = /(?<=new\s\=\s)[a-z0-9\s\+\*\-\/]+(?=\n)/;
     monkey.operation = monkeyString.match(operationRegex)[0];
 
-    // divisor
     const divisorRegex = /(?<=divisible by )[0-9]+(?=\n)/;
     monkey.divisor = Number(monkeyString.match(divisorRegex)[0]);
 
-    // actions if true or false
     const trueRegex = /(?<=true: throw to monkey )[0-9]+(?=\n)/;
     const falseRegex = /(?<=false: throw to monkey )[0-9]+/;
 
@@ -33,8 +27,6 @@ const monkeys = monkeysStrings.map((monkeyString) => {
     return monkey;
 });
 
-// loop through all monkeys twenty times
-// loop through each monkey's items in turn, processing as necessary, doing ++objectsInspected.
 for (let i = 0; i < 20; ++i) {
     monkeys.forEach((monkey) => {
         monkey.startingItems.forEach((startingItem) => {
@@ -52,7 +44,6 @@ for (let i = 0; i < 20; ++i) {
     });
 }
 
-// identify two monkeys with highest level of inspected objects
 const objectsInspected = monkeys.map((monkey) => {
     return monkey.objectsInspected;
 });
