57e1b2c9e2e0c7c96e111a1d40bc787598bb21b7
a1v0 <avc285@gmail.com>
2023-05-31 14:21:39 +0100

    Move identification of extremities to new method

--- 14/1-how-many-units-of-sand.js
+++ 14/1-how-many-units-of-sand.js
@@ -29,14 +29,7 @@ blockers.forEach((blocker) => {
             blocker[i][0] !== blocker[i + 1][0] ? 0 : 1
         );
 
-        // identify extremities of grid
-        if (blocker[i][0] < leftmost) leftmost = blocker[i][0];
-        if (blocker[i + 1][0] < leftmost) leftmost = blocker[i + 1][0];
-        if (blocker[i][0] > rightmost) rightmost = blocker[i][0];
-        if (blocker[i + 1][0] > rightmost) rightmost = blocker[i + 1][0];
-
-        if (blocker[i][1] > lowest) lowest = blocker[i][1];
-        if (blocker[i + 1][1] > lowest) lowest = blocker[i + 1][1];
+        identifyExtremities(blocker,i)
     }
 });
 
@@ -72,6 +65,16 @@ while (currentX >= leftmost && currentX <= rightmost && currentY < lowest) {
 }
 
 console.log(restingGrainsCounter);
+
+function identifyExtremities(blocker,i){
+        if (blocker[i][0] < leftmost) leftmost = blocker[i][0];
+        if (blocker[i + 1][0] < leftmost) leftmost = blocker[i + 1][0];
+        if (blocker[i][0] > rightmost) rightmost = blocker[i][0];
+        if (blocker[i + 1][0] > rightmost) rightmost = blocker[i + 1][0];
+
+        if (blocker[i][1] > lowest) lowest = blocker[i][1];
+        if (blocker[i + 1][1] > lowest) lowest = blocker[i + 1][1];
+}
 
 function addBlockers(start, end, xOrY) {
     // needs to take into account whether it goes up, down, left or right
