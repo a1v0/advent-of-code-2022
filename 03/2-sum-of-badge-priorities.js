6c7649367f5ff07c7ec5638240f7e35117b9b666
a1v0 <avc285@gmail.com>
2023-05-31 10:51:14 +0100

    Remove comments

--- 03/2-sum-of-badge-priorities.js
+++ 03/2-sum-of-badge-priorities.js
@@ -1,11 +1,7 @@
 const input = require("./input.js");
 
-// my solution involves lots of iteration. I do this to make the code obvious, though it is by no means efficient
-
-// split input text into array of rucksacks
 const rucksackStrings = input.split("\n");
 
-// map array into array of objects: { firstRucksack: "", secondRucksack: "", thirdRucksack: "" }
 const rucksackGroups = [];
 for (let i = 0; i < rucksackStrings.length; i += 3) {
     rucksackGroups.push({
@@ -15,7 +11,6 @@ for (let i = 0; i < rucksackStrings.length; i += 3) {
     });
 }
 
-// identify badge of each group
 rucksackGroups.forEach((rucksackGroup) => {
     for (let letter of rucksackGroup.first) {
         if (rucksackGroup.second.includes(letter)) {
@@ -27,7 +22,6 @@ rucksackGroups.forEach((rucksackGroup) => {
     }
 });
 
-// create array of totals based on priority values for each group's badge
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const priorityTotals = rucksackGroups.map((rucksackGroup) => {
     return letters.indexOf(rucksackGroup.badge) + 1;
