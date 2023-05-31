ff2ecd0c9f8bf4b7309e987088f51b9eedede1da
a1v0 <avc285@gmail.com>
2023-05-31 14:07:31 +0100

    Remove comments

--- 13/2-what-is-decoder-key.js
+++ 13/2-what-is-decoder-key.js
@@ -1,9 +1,7 @@
 const { input } = require("./input");
 
-// split input into pairs of strings
 const packetsStrings = input.split("\n\n");
 
-// map pairs into an array of arrays, using eval()
 const packets = packetsStrings.map((packetString) => {
     const splitPackets = packetString.split("\n");
     const packets = splitPackets.map((splitPacket) => {
@@ -16,20 +14,22 @@ const allPacketsInOnePile = [];
 packets.forEach((packet) => {
     allPacketsInOnePile.push(packet[0], packet[1]);
 });
+
 const firstDividerPacket = [[2]],
     secondDividerPacket = [[6]];
 allPacketsInOnePile.push(firstDividerPacket, secondDividerPacket);
 
-// create compare function take takes two arrays
 function comparePackets(leftPacket, rightPacket) {
     const loopLength =
         leftPacket.length > rightPacket.length
             ? leftPacket.length
             : rightPacket.length;
+    
     for (let i = 0; i < loopLength; ++i) {
         // if one side has run out of numbers
         if (leftPacket[i] === undefined) return -1;
         else if (rightPacket[i] === undefined) return 1;
+        
         // if both are numbers, compare
         else if (
             typeof leftPacket[i] === "number" &&
@@ -41,18 +41,20 @@ function comparePackets(leftPacket, rightPacket) {
             else if (leftPacket[i] < rightPacket[i]) return -1;
             // else continue
         }
+        
         // if one is array
         else if (
             Array.isArray(leftPacket[i]) ||
             Array.isArray(rightPacket[i])
         ) {
-            // coerce other to array (if it isn't already an array)
             leftPacket[i] = Array.isArray(leftPacket[i])
                 ? leftPacket[i]
                 : [leftPacket[i]];
+           
             rightPacket[i] = Array.isArray(rightPacket[i])
                 ? rightPacket[i]
                 : [rightPacket[i]];
+            
             // run function recursively
             const result = comparePackets(leftPacket[i], rightPacket[i]);
             if (result !== undefined) return result;
@@ -60,10 +62,10 @@ function comparePackets(leftPacket, rightPacket) {
     }
 }
 
-// loop through packets invoking the compare function each time
 allPacketsInOnePile.sort((left, right) => {
     return comparePackets(left, right);
 });
+
 console.log(
     (allPacketsInOnePile.indexOf(firstDividerPacket) + 1) *
         (allPacketsInOnePile.indexOf(secondDividerPacket) + 1)
