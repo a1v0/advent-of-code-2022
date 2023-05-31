7d19f165726419fbb475e111b6cc77fcb064c8d7
a1v0 <avc285@gmail.com>
2023-05-31 14:03:25 +0100

    Remove comments

--- 13/1-sum-of-indices.js
+++ 13/1-sum-of-indices.js
@@ -1,9 +1,7 @@
 const { input } = require("./input");
 
-// split input into pairs of strings
 const packetsStrings = input.split("\n\n");
 
-// map pairs into an array of arrays, using eval()
 const packets = packetsStrings.map((packetString) => {
     const splitPackets = packetString.split("\n");
     const packets = splitPackets.map((splitPacket) => {
@@ -12,21 +10,21 @@ const packets = packetsStrings.map((packetString) => {
     return packets;
 });
 
-// create array to store indexes of items in correct order
 const correctOrderIndices = [];
 
-// create compare function take takes two arrays and index as argument
 function comparePackets(leftPacket, rightPacket, index) {
     const loopLength =
         leftPacket.length > rightPacket.length
             ? leftPacket.length
             : rightPacket.length;
     for (let i = 0; i < loopLength; ++i) {
+        
         // if one side has run out of numbers
         if (leftPacket[i] === undefined) {
             correctOrderIndices.push(index);
             return true;
         } else if (rightPacket[i] === undefined) return false;
+        
         // if both are numbers, compare
         else if (
             typeof leftPacket[i] === "number" &&
@@ -39,8 +37,8 @@ function comparePackets(leftPacket, rightPacket, index) {
                 correctOrderIndices.push(index);
                 return true;
             }
-            // else continue
         }
+        
         // if one is array
         else if (
             Array.isArray(leftPacket[i]) ||
@@ -53,6 +51,7 @@ function comparePackets(leftPacket, rightPacket, index) {
             rightPacket[i] = Array.isArray(rightPacket[i])
                 ? rightPacket[i]
                 : [rightPacket[i]];
+            
             // run function recursively
             const result = comparePackets(leftPacket[i], rightPacket[i], index);
             if (result !== undefined) return result;
@@ -60,12 +59,10 @@ function comparePackets(leftPacket, rightPacket, index) {
     }
 }
 
-// loop through packets invoking the compare function each time
 packets.forEach((packet, index) => {
     comparePackets(packet[0], packet[1], index);
 });
 
-// count indices of arrays in the right order
 const sumCorrectOrderIndices = correctOrderIndices.reduce(
     (total, currentIndex) => {
         return total + currentIndex + 1; // +1 because the indices are not zero-indexed
