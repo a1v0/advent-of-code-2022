8e431d717b79300a962899badffee079c2d7379e
a1v0 <avc285@gmail.com>
2023-05-31 21:32:48 +0100

    Remove comments

--- 06/2-start-of-message-marker.js
+++ 06/2-start-of-message-marker.js
@@ -1,7 +1,5 @@
 const { input } = require("./input.js");
 
-// may God have mercy on the inefficiency of this...
-
 // need to find the no. of characters right up to the end of the first fourteen-character start-of-message marker
 
 // map string into overlapping substrings
@@ -14,7 +12,6 @@ for (let i = 0; i < input.length; ++i) {
     groupsOfFourteen.push(groupOfFourteen);
 }
 
-// loop through each substring, checking if indexOf of each letter === lastIndexOf of each letter
 function isUnique(string, value) {
     return string.indexOf(value) === string.lastIndexOf(value);
 }
@@ -34,5 +31,4 @@ for (let i = 0; i < groupsOfFourteen.length; ++i) {
     }
 }
 
-// log index of the first substring that passes the test, then return index + 4
 console.log(startOfMessageIndex + 14);
