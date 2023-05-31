dc30e338f846aa87935a8ccb872cbf734ad55d54
a1v0 <avc285@gmail.com>
2023-05-31 15:18:36 +0100

    Remove comments

--- 21/1-what-number-will-root-yell.js
+++ 21/1-what-number-will-root-yell.js
@@ -1,10 +1,8 @@
 const { input } = require("./input");
 
 function day21Task1(input) {
-    // split input into array of strings
     const inputStrings = input.split("\n");
 
-    // map strings into object
     const monkeys = {};
     inputStrings.forEach((inputString) => {
         const monkeyName = inputString.match(/^\w{4}/)[0];
@@ -12,17 +10,13 @@ function day21Task1(input) {
         monkeys[monkeyName] = [monkeyCry, false];
     });
 
-    // while loop until root item is a number
     const quartetRegex = /\w{4}/;
     while (!monkeys.root[1]) {
-        // loop through monkeys, replacing strings as necessary
         for (let quartet in monkeys) {
             try {
-                // use eval() to update object, set isNumber to true
                 monkeys[quartet][0] = String(eval(monkeys[quartet][0]));
                 monkeys[quartet][1] = true;
 
-                // find quartet that uses that value and update
                 for (let quartetSearch in monkeys) {
                     if (RegExp(quartet).test(monkeys[quartetSearch][0])) {
                         monkeys[quartetSearch][0] = monkeys[
@@ -36,5 +30,6 @@ function day21Task1(input) {
     }
     return Number(monkeys.root[0]);
 }
+
 // console.log(day21Task1(input));
 module.exports = { day21Task1 };
