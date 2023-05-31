215b1fc55acd526e150c2af8c7fafe240d5ca88b
a1v0 <avc285@gmail.com>
2023-05-31 10:13:24 +0100

    Tidy code, remove comments, modify replace() method

--- 02/1-total-score.js
+++ 02/1-total-score.js
@@ -2,23 +2,20 @@ const { input } = require("./input.js");
 
 // convert XYZ into ABC for ease of comparison
 let allABCs = input;
-allABCs = allABCs.replace(/X/g, "A");
-allABCs = allABCs.replace(/Y/g, "B");
-allABCs = allABCs.replace(/Z/g, "C");
+allABCs = allABCs.replaceAll("X", "A");
+allABCs = allABCs.replaceAll("Y", "B");
+allABCs = allABCs.replaceAll("Z", "C");
 
-// split input into individual rounds
 const roundsStrings = allABCs.split("\n");
 const rounds = roundsStrings.map((roundString) => {
     return roundString.split(" ");
 });
 
-// create three variables for wins, losses and draws, and an array to store methods of attack
 let wins = 0,
     losses = 0,
     draws = 0;
 const attackMethods = [];
 
-// loop through array of games. If it's a draw, for example, add my attack to the draws array
 rounds.forEach((round) => {
     const elfAttack = round[0];
     const myAttack = round[1];
@@ -36,15 +33,12 @@ rounds.forEach((round) => {
     attackMethods.push(myAttack);
 });
 
-// create total score variable
 let totalScore = 0;
 
-// add points for wins/draws/losses
 totalScore += wins * 6;
 totalScore += losses * 0;
 totalScore += draws * 3;
 
-// add points for types of method
 attackMethods.forEach((attackMethod) => {
     if (attackMethod === "A") {
         totalScore += 1;
@@ -56,5 +50,3 @@ attackMethods.forEach((attackMethod) => {
 });
 
 console.log(totalScore);
-
-// it's not efficient, but it'll keep the code clean. I may refactor it
