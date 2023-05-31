9e05f637afef79a1d56cff42c4ce9771284b36c7
a1v0 <avc285@gmail.com>
2023-05-31 11:25:38 +0100

    Set startOfPacketIndex from a separate method

--- 06/1-start-of-packet-marker.js
+++ 06/1-start-of-packet-marker.js
@@ -17,18 +17,20 @@ for (let i = 0; i < input.length; ++i) {
 
 // loop through each substring, checking if indexOf of each letter === lastIndexOf of each letter
 
-let startOfPacketIndex;
-for (let i = 0; i < groupsOfFour.length; ++i) {
-    if (groupsOfFour[i].indexOf(groupsOfFour[i][0]) !==
-            groupsOfFour[i].lastIndexOf(groupsOfFour[i][0])) continue;
-    if (groupsOfFour[i].indexOf(groupsOfFour[i][1]) !==
-            groupsOfFour[i].lastIndexOf(groupsOfFour[i][1])) continue;
-    if (groupsOfFour[i].indexOf(groupsOfFour[i][2]) !==
-            groupsOfFour[i].lastIndexOf(groupsOfFour[i][2])) continue;
-    
-        startOfPacketIndex = i;
-        break;
-}
+const startOfPacketIndex = getStartOfPacketIndex(groupsOfFour);
 
 // log index of the first substring that passes the test, then return index + 4
 console.log(startOfPacketIndex + 4);
+
+const getStartOfPacketIndex = (groupsOfFour) => {
+    for (let i = 0; i < groupsOfFour.length; ++i) {
+        if (groupsOfFour[i].indexOf(groupsOfFour[i][0]) !==
+                groupsOfFour[i].lastIndexOf(groupsOfFour[i][0])) continue;
+        if (groupsOfFour[i].indexOf(groupsOfFour[i][1]) !==
+                groupsOfFour[i].lastIndexOf(groupsOfFour[i][1])) continue;
+        if (groupsOfFour[i].indexOf(groupsOfFour[i][2]) !==
+                groupsOfFour[i].lastIndexOf(groupsOfFour[i][2])) continue;
+
+            return i;
+    }
+};
