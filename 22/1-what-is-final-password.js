766c0fa713e512ba99704e1e57c170c6e6d4235d
a1v0 <avc285@gmail.com>
2023-06-03 22:52:12 +0100

    Move move() to end of file and add params

--- 22/1-what-is-final-password.js
+++ 22/1-what-is-final-password.js
@@ -1,10 +1,9 @@
 const { input } = require("../22/input");
 
 function day22Task1(input) {
-    
-    const instructionsStringRegex=/\w+/
-          const instructionsString = input.match(instructionsStringRegex)[0];
-const instructionsRegex=/\d+|\D/g
+    const instructionsStringRegex = /\w+/;
+    const instructionsString = input.match(instructionsStringRegex)[0];
+    const instructionsRegex = /\d+|\D/g;
     const instructions = instructionsString
         .match(instructionsRegex)
         .map((instruction) => {
@@ -14,7 +13,7 @@ const instructionsRegex=/\d+|\D/g
             return instruction;
         });
 
-    const boardStringRegex=/^[\s\#\.]+(?=\n\n)/
+    const boardStringRegex = /^[\s\#\.]+(?=\n\n)/;
     const boardString = input.match(boardStringRegex)[0];
 
     const board = [];
@@ -26,132 +25,15 @@ const instructionsRegex=/\d+|\D/g
     const startingX = board[0].indexOf(".");
     const currentCoordinates = [startingX, 0];
 
-    
-    let direction = 0;// value % 4 defines direction (right: 0, down: 1, left: 2, up: 3)
-    
-    
+    let direction = 0; // value % 4 defines direction (right: 0, down: 1, left: 2, up: 3)
+
     for (let instruction of instructions) {
         if (isNaN(instruction)) {
             direction += instruction === "L" ? -1 : 1;
             continue;
         }
 
-        move(instruction);
-    }
-
-    function move(instruction) {
-        const directionValue =
-            direction < 0 && direction % 4 != 0
-                ? (direction % 4) + 4
-                : Math.abs(direction % 4);
-
-        // this code is very repetitive. I'd love to know how to reduce that
-        for (let i = 0; i < instruction; ++i) {
-            // going right
-            if (directionValue === 0) {
-                if (
-                    currentCoordinates[0] + 1 >=
-                        board[currentCoordinates[1]].length ||
-                    board[currentCoordinates[1]][currentCoordinates[0] + 1] ===
-                        " "
-                ) {
-                    // if it's undefined or a space, do a wrap, provided it's not # on the other side
-                    if (
-                        !board[currentCoordinates[1]].includes("#") ||
-                        board[currentCoordinates[1]].indexOf(".") <
-                            board[currentCoordinates[1]].indexOf("#")
-                    ) {
-                        currentCoordinates[0] =
-                            board[currentCoordinates[1]].indexOf(".");
-                    }
-                } else if (
-                    board[currentCoordinates[1]][currentCoordinates[0] + 1] ===
-                    "."
-                ) {
-                    ++currentCoordinates[0];
-                } else {
-                    break;
-                }
-            }
-
-            // going left
-            else if (directionValue === 2) {
-                if (
-                    currentCoordinates[0] - 1 < 0 ||
-                    board[currentCoordinates[1]][currentCoordinates[0] - 1] ===
-                        " "
-                ) {
-                    // if it's undefined or a space, do a wrap, provided it's not # on the other side
-                    if (
-                        board[currentCoordinates[1]].lastIndexOf(".") >
-                        board[currentCoordinates[1]].lastIndexOf("#")
-                    ) {
-                        currentCoordinates[0] =
-                            board[currentCoordinates[1]].lastIndexOf(".");
-                    }
-                } else if (
-                    board[currentCoordinates[1]][currentCoordinates[0] - 1] ===
-                    "."
-                ) {
-                    --currentCoordinates[0];
-                } else {
-                    break;
-                }
-            }
-
-            // going down
-            else if (directionValue === 1) {
-                if (
-                    currentCoordinates[1] + 1 >= board.length ||
-                    board[currentCoordinates[1] + 1][currentCoordinates[0]] ===
-                        " " ||
-                    board[currentCoordinates[1] + 1][currentCoordinates[0]] ===
-                        undefined
-                ) {
-                    for (let j = 0; j < board.length; ++j) {
-                        if (board[j][currentCoordinates[0]] === "#") {
-                            break;
-                        }
-                        if (board[j][currentCoordinates[0]] === ".") {
-                            currentCoordinates[1] = j;
-                            break;
-                        }
-                    }
-                } else if (
-                    board[currentCoordinates[1] + 1][currentCoordinates[0]] ===
-                    "."
-                ) {
-                    ++currentCoordinates[1];
-                } else {
-                    break;
-                }
-            }
-
-            // going up
-            else if (directionValue === 3) {
-                if (
-                    currentCoordinates[1] - 1 < 0 ||
-                    board[currentCoordinates[1] - 1][currentCoordinates[0]] ===
-                        " "
-                ) {
-                    for (let j = board.length - 1; j >= 0; --j) {
-                        if (board[j][currentCoordinates[0]] === "#") {
-                            break;
-                        }
-                        if (board[j][currentCoordinates[0]] === ".") {
-                            currentCoordinates[1] = j;
-                            break;
-                        }
-                    }
-                } else if (
-                    board[currentCoordinates[1] - 1][currentCoordinates[0]] ===
-                    "."
-                ) { --currentCoordinates[1];
-                } else {
-                    break;
-                }
-            }
-        }
+        move(instruction, direction, currentCoordinates, board);
     }
 
     const multipliedRow = (currentCoordinates[1] + 1) * 1000, // + 1 because rows in the task aren't zero-indexed
@@ -163,5 +45,115 @@ const instructionsRegex=/\d+|\D/g
     return multipliedRow + multipliedColumn + directionValue;
 }
 
-// console.log(day22Task1(input));
+console.log(day22Task1(input));
+
+function move(instruction, direction, currentCoordinates, board) {
+    const directionValue =
+        direction < 0 && direction % 4 != 0
+            ? (direction % 4) + 4
+            : Math.abs(direction % 4);
+
+    // this code is very repetitive. I'd love to know how to reduce that
+    for (let i = 0; i < instruction; ++i) {
+        // going right
+        if (directionValue === 0) {
+            if (
+                currentCoordinates[0] + 1 >=
+                    board[currentCoordinates[1]].length ||
+                board[currentCoordinates[1]][currentCoordinates[0] + 1] === " "
+            ) {
+                // if it's undefined or a space, do a wrap, provided it's not # on the other side
+                if (
+                    !board[currentCoordinates[1]].includes("#") ||
+                    board[currentCoordinates[1]].indexOf(".") <
+                        board[currentCoordinates[1]].indexOf("#")
+                ) {
+                    currentCoordinates[0] =
+                        board[currentCoordinates[1]].indexOf(".");
+                }
+            } else if (
+                board[currentCoordinates[1]][currentCoordinates[0] + 1] === "."
+            ) {
+                ++currentCoordinates[0];
+            } else {
+                break;
+            }
+        }
+
+        // going left
+        else if (directionValue === 2) {
+            if (
+                currentCoordinates[0] - 1 < 0 ||
+                board[currentCoordinates[1]][currentCoordinates[0] - 1] === " "
+            ) {
+                // if it's undefined or a space, do a wrap, provided it's not # on the other side
+                if (
+                    board[currentCoordinates[1]].lastIndexOf(".") >
+                    board[currentCoordinates[1]].lastIndexOf("#")
+                ) {
+                    currentCoordinates[0] =
+                        board[currentCoordinates[1]].lastIndexOf(".");
+                }
+            } else if (
+                board[currentCoordinates[1]][currentCoordinates[0] - 1] === "."
+            ) {
+                --currentCoordinates[0];
+            } else {
+                break;
+            }
+        }
+
+        // going down
+        else if (directionValue === 1) {
+            if (
+                currentCoordinates[1] + 1 >= board.length ||
+                board[currentCoordinates[1] + 1][currentCoordinates[0]] ===
+                    " " ||
+                board[currentCoordinates[1] + 1][currentCoordinates[0]] ===
+                    undefined
+            ) {
+                for (let j = 0; j < board.length; ++j) {
+                    if (board[j][currentCoordinates[0]] === "#") {
+                        break;
+                    }
+                    if (board[j][currentCoordinates[0]] === ".") {
+                        currentCoordinates[1] = j;
+                        break;
+                    }
+                }
+            } else if (
+                board[currentCoordinates[1] + 1][currentCoordinates[0]] === "."
+            ) {
+                ++currentCoordinates[1];
+            } else {
+                break;
+            }
+        }
+
+        // going up
+        else if (directionValue === 3) {
+            if (
+                currentCoordinates[1] - 1 < 0 ||
+                board[currentCoordinates[1] - 1][currentCoordinates[0]] === " "
+            ) {
+                for (let j = board.length - 1; j >= 0; --j) {
+                    if (board[j][currentCoordinates[0]] === "#") {
+                        break;
+                    }
+                    if (board[j][currentCoordinates[0]] === ".") {
+                        currentCoordinates[1] = j;
+                        break;
+                    }
+                }
+            } else if (
+                board[currentCoordinates[1] - 1][currentCoordinates[0]] === "."
+            ) {
+                --currentCoordinates[1];
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 module.exports = { day22Task1 };
