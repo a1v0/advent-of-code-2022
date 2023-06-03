72ed468679970115093a7526cfa85a975bfe1c83
a1v0 <avc285@gmail.com>
2023-06-03 22:23:22 +0100

    Rename coordinates

--- 09/2-ten-knots-in-rope.js
+++ 09/2-ten-knots-in-rope.js
@@ -6,87 +6,95 @@ const instructions = instructionLines.map((instructionLine) => {
     return instructionLine.split(" ");
 });
 
-const visitedByTail = {};// contents formatted like this: { "x,y": true, "a,b": true }
+const visitedByTail = {}; // contents formatted like this: { "x,y": true, "a,b": true }
 
-// coordinates now an array going from head to tail in order
-const coordinates = [];
+const ropeKnotCoordinates = []; // in order from head to tail
 
-for(let i=0;i<10;++i){
-coordinates.push([0,0]);}
+for (let i = 0; i < 10; ++i) {
+    ropeKnotCoordinates.push([0, 0]);
+}
 
 instructions.forEach(([direction, distance]) => {
     for (let i = 0; i < distance; ++i) {
         switch (direction) {
             case "U":
-                ++coordinates[0][1];
+                ++ropeKnotCoordinates[0][1];
                 break;
             case "D":
-                --coordinates[0][1];
+                --ropeKnotCoordinates[0][1];
                 break;
             case "L":
-                --coordinates[0][0];
+                --ropeKnotCoordinates[0][0];
                 break;
             case "R":
-                ++coordinates[0][0];
+                ++ropeKnotCoordinates[0][0];
                 break;
         }
 
-        for (let j = 0; j < coordinates.length - 1; ++j) {
+        for (let j = 0; j < ropeKnotCoordinates.length - 1; ++j) {
             if (
-                coordinates[j][0] - coordinates[j + 1][0] < -1 ||
-                coordinates[j][0] - coordinates[j + 1][0] > 1 ||
-                coordinates[j][1] - coordinates[j + 1][1] < -1 ||
-                coordinates[j][1] - coordinates[j + 1][1] > 1
+                ropeKnotCoordinates[j][0] - ropeKnotCoordinates[j + 1][0] <
+                    -1 ||
+                ropeKnotCoordinates[j][0] - ropeKnotCoordinates[j + 1][0] > 1 ||
+                ropeKnotCoordinates[j][1] - ropeKnotCoordinates[j + 1][1] <
+                    -1 ||
+                ropeKnotCoordinates[j][1] - ropeKnotCoordinates[j + 1][1] > 1
             ) {
                 // if they share same y
-                if (coordinates[j][1] === coordinates[j + 1][1]) {
-                    coordinates[j][0] < coordinates[j + 1][0]
-                        ? --coordinates[j + 1][0]
-                        : ++coordinates[j + 1][0];
+                if (
+                    ropeKnotCoordinates[j][1] === ropeKnotCoordinates[j + 1][1]
+                ) {
+                    ropeKnotCoordinates[j][0] < ropeKnotCoordinates[j + 1][0]
+                        ? --ropeKnotCoordinates[j + 1][0]
+                        : ++ropeKnotCoordinates[j + 1][0];
                 }
                 // if they share the same x
-                else if (coordinates[j][0] === coordinates[j + 1][0]) {
-                    coordinates[j][1] < coordinates[j + 1][1]
-                        ? --coordinates[j + 1][1]
-                        : ++coordinates[j + 1][1];
+                else if (
+                    ropeKnotCoordinates[j][0] === ropeKnotCoordinates[j + 1][0]
+                ) {
+                    ropeKnotCoordinates[j][1] < ropeKnotCoordinates[j + 1][1]
+                        ? --ropeKnotCoordinates[j + 1][1]
+                        : ++ropeKnotCoordinates[j + 1][1];
                 }
                 // up right
                 else if (
-                    coordinates[j][0] > coordinates[j + 1][0] &&
-                    coordinates[j][1] > coordinates[j + 1][1]
+                    ropeKnotCoordinates[j][0] > ropeKnotCoordinates[j + 1][0] &&
+                    ropeKnotCoordinates[j][1] > ropeKnotCoordinates[j + 1][1]
                 ) {
-                    ++coordinates[j + 1][0];
-                    ++coordinates[j + 1][1];
+                    ++ropeKnotCoordinates[j + 1][0];
+                    ++ropeKnotCoordinates[j + 1][1];
                 }
                 // up left
                 else if (
-                    coordinates[j][0] < coordinates[j + 1][0] &&
-                    coordinates[j][1] > coordinates[j + 1][1]
+                    ropeKnotCoordinates[j][0] < ropeKnotCoordinates[j + 1][0] &&
+                    ropeKnotCoordinates[j][1] > ropeKnotCoordinates[j + 1][1]
                 ) {
-                    --coordinates[j + 1][0];
-                    ++coordinates[j + 1][1];
+                    --ropeKnotCoordinates[j + 1][0];
+                    ++ropeKnotCoordinates[j + 1][1];
                 }
                 // down right
                 else if (
-                    coordinates[j][0] > coordinates[j + 1][0] &&
-                    coordinates[j][1] < coordinates[j + 1][1]
+                    ropeKnotCoordinates[j][0] > ropeKnotCoordinates[j + 1][0] &&
+                    ropeKnotCoordinates[j][1] < ropeKnotCoordinates[j + 1][1]
                 ) {
-                    ++coordinates[j + 1][0];
-                    --coordinates[j + 1][1];
+                    ++ropeKnotCoordinates[j + 1][0];
+                    --ropeKnotCoordinates[j + 1][1];
                 }
                 // down left
                 else if (
-                    coordinates[j][0] < coordinates[j + 1][0] &&
-                    coordinates[j][1] < coordinates[j + 1][1]
+                    ropeKnotCoordinates[j][0] < ropeKnotCoordinates[j + 1][0] &&
+                    ropeKnotCoordinates[j][1] < ropeKnotCoordinates[j + 1][1]
                 ) {
-                    --coordinates[j + 1][0];
-                    --coordinates[j + 1][1];
-                } 
+                    --ropeKnotCoordinates[j + 1][0];
+                    --ropeKnotCoordinates[j + 1][1];
+                }
             }
         }
-        visitedByTail[`${coordinates[9][0]},${coordinates[9][1]}`] = true;
+        visitedByTail[
+            `${ropeKnotCoordinates[9][0]},${ropeKnotCoordinates[9][1]}`
+        ] = true;
     }
 });
 
-const noOfLocationsVisited=Object.keys(visitedByTail).length
+const noOfLocationsVisited = Object.keys(visitedByTail).length;
 console.log(noOfLocationsVisited);
