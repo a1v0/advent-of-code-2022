918c81a8fcde117a41b0329dc036045181e0b8ee
a1v0 <avc285@gmail.com>
2023-08-22 20:38:47 +0100

    Create buildBlueprints method

--- 19/1-total-quality-level.js
+++ 19/1-total-quality-level.js
@@ -23,9 +23,8 @@ const { input } = require("./input");
 // Do the addition etc.
 
 const day19Task1 = (input) => {
-    // split input into rows
-    // map rows into array of blueprint objects detailing the costs
-    //    e.g. { obsidian: { ore: 2, clay: 3 } }
+    const blueprints = buildBlueprints(input);
+
     // create array to house max number of geodes per blueprint
     // loop through all blueprints
     //    add a console log every time a new blueprint is opened
@@ -40,6 +39,20 @@ const day19Task1 = (input) => {
     //    possibly add a console log here (unless it's cluttered, in which case perhaps just do it every time we update the max. geodes value)
     //
     // determine quality level
+};
+
+const buildBlueprints = (input) => {
+    const blueprintRows = input.split("\n");
+    const blueprints = blueprintRows.map();
+
+    return blueprints;
+};
+
+const parseBlueprintRow = (blueprintRow) => {
+    // Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+    // Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+    // map rows into array of blueprint objects detailing the costs
+    //    e.g. { obsidian: { ore: 2, clay: 3 } }
 };
 
 console.log(day19Task1(input));
