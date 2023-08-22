5125b066cbb4780812c9f252ac5f65122054e2c3
a1v0 <avc285@gmail.com>
2023-08-22 20:23:26 +0100

    Add high-level plan

--- 19/1-total-quality-level.js
+++ 19/1-total-quality-level.js
@@ -1,8 +1,24 @@
-const { testInput: input } = require("./input");
+const { input } = require("./input");
 
-// split input into rows
-// map rows into array of blueprint objects
-// need to do some maths to work out the ratio of ore to clay robots needed. not sure how that'll work
-// loop through each blueprint for 24 minutes, keeping track of inventories
-// // this needs to be smart, somehow, not random. hopefully the maths from above will help
-// calculate quality level
+// Trial and error algo
+// Loop over all blueprints
+//     Add a console log every time a new blueprint starts
+// Backtracking/recursion
+//     Given we’re running for just 20-odd minutes, we shouldn’t run into problems re: reaching the stack limit
+//     However, it might take a while to run, since it’s essentially a brute-force method that has to evaluate every single possibility
+//         Is there a way to filter out ones that are unlikely to yield anything good?
+// Evaluate every single combination by doing the following every time:
+//     Build every type of robot you can
+//     Build nothing
+// When we reach the maximum amount of minutes, update the maximum number of geodes, then return
+//     Possibly add a log every time we reach this point, just so we know how fast/slow the script is running
+//     If it’s cluttered, then perhaps every time the max number is updated
+// Do the addition etc.
+const day19Task1 = (input) => {
+    // split input into rows
+    // map rows into array of blueprint objects
+};
+
+console.log(day19Task1(input));
+
+module.exports = { day19Task1 };
